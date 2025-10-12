9e0c25645296a0327ab0da3611c67e94c739796f
Garvish Jain <garvishjain1997@gmail.com>
2025-10-12 18:05:55 +0530

    some add in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: ['workbridge-z5ek.onrender.com'], // ✅ Add your backend domain here
+  }
 })
